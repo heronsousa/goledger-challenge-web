2c95dad91c6953a7c0f1f91c0fc46bbfdb1c754f
Heron Rodrigues <heronrs1@gmail.com>
2020-02-20 17:19:17 -0300

    delete contact

--- src/App.js
+++ src/App.js
@@ -7,30 +7,47 @@ function App() {
   const [contacts, setContacts] = useState([]);
 
   useEffect(() => {
-    try {
-      async function getContacts() {
-        const response = await api.post('/search', 
-          {
-            "selector": {
-              "@assetType": "contact"
-            }
-          }
-        );
-
-        setContacts(response.data.result);
-      }
-
-      getContacts();
-    } catch (error) {
-      console.log(error);
+    async function getContacts() {
+      const selector = { "@assetType": "contact" };
+      const response = await api.post('/search', { selector });
+      
+      setContacts(response.data.result);
     }
-  });
+    
+    getContacts();
+  }, []);
+  
+  async function deleteContact(name){
+    const data = {
+      "@assetType": "contact",
+      "name": name
+    }
 
+    const response = await api.delete('/delete',{ data });
+
+    console.log(response);
+
+    window.location.reload(false);
+  }
 
   return (
-    <div className="App">
-      GoLedger Challenge
-    </div>
+    <>
+      <div className="App">
+        GoLedger Challenge
+      </div>
+
+      {contacts.map( contact => (
+        <ul key={contact['@key']}>
+          <li>Nome: {contact.name}</li>
+          <li>Telefone: {contact.phone}</li>
+          <li>Email: {contact.email}</li>
+          <li>Companhia: {contact.company}</li>
+          <li>Idade: {contact.age}</li>
+
+          <button onClick={() => {deleteContact(contact.name)}}>Delete</button>
+        </ul>
+      ))}
+    </>
   );
 }
 
