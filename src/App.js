838cc9cf1db57e5132eadfeda9676c360e40382f
Heron Rodrigues <heronrs1@gmail.com>
2020-02-20 16:23:31 -0300

    accessing api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import api from './services/api';
 
 function App() {
+
+  const [contacts, setContacts] = useState([]);
+
+  useEffect(() => {
+    try {
+      async function getContacts() {
+        const response = await api.post('/search', 
+          {
+            "selector": {
+              "@assetType": "contact"
+            }
+          }
+        );
+
+        setContacts(response.data.result);
+      }
+
+      getContacts();
+    } catch (error) {
+      console.log(error);
+    }
+  });
+
+
   return (
     <div className="App">
-    
+      GoLedger Challenge
     </div>
   );
 }
