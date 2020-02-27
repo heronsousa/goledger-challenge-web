f8b0d71e89872615156f9975a3d1c18eb8be3811
Heron Rodrigues <heronrs1@gmail.com>
2020-02-26 22:36:49 -0300

    adding responsive

--- src/pages/Menu/index.js
+++ src/pages/Menu/index.js
@@ -7,30 +7,33 @@ import './styles.css';
 
 export default function Menu() {
 
-  const [open, setOpen] = useState(false);
+    const [open, setOpen] = useState(false);
 
-  const contact = {
-    "name": '',
-    "email": '',
-    "age": 0,
-    "company": '',
-    "phone": ''
-  };
-  const title = 'Adicionar';
+    const contact = {
+        "name": '',
+        "email": '',
+        "age": 0,
+        "company": '',
+        "phone": ''
+    };
+    const title = 'Adicionar';
 
-  return (
-    <>
-      <div className="menu-item" onClick={() => {window.location.reload(false)}}>
-        <PeopleAltOutlined />
-        <strong>Todos os contatos</strong>
-      </div>
-      
-      <div className="menu-item" onClick={() => {setOpen(true)}}>
-        <PersonAddOutlined />
-        <strong>Adicionar contato</strong>
-      </div>
+    return (
 
-      <ContactForm modal={{ open, setOpen, contact, title }} />
-    </>
-  );
+        <div className="menu">
+
+            <div className="menu-item" onClick={() => { window.location.reload(false) }}>
+                <PeopleAltOutlined />
+                <strong>Todos os contatos</strong>
+            </div>
+
+            <div className="menu-item" onClick={() => { setOpen(true) }}>
+                <PersonAddOutlined />
+                <strong>Adicionar contato</strong>
+            </div>
+
+            <ContactForm modal={{ open, setOpen, contact, title }} />
+
+        </div>
+    );
 }
